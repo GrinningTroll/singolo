b9885d58b4e6252e22c7f7864a3910a3a40d7a2c
GrinningTroll <kostya_mackevich@mail.ru>
2020-03-29 15:33:04 +0300

    add: responsive in css and responsive in js

--- script.js
+++ script.js
@@ -7,15 +7,44 @@ function onScroll(event) {
 
   sectors.forEach(el => {
     if (el.offsetTop <= curPos && el.offsetTop + el.offsetHeight > curPos) {
-      console.log(el.offsetTop + el.offsetHeight);
+      //console.log(el.offsetTop + el.offsetHeight);
 
       links.forEach(a => {
         a.classList.remove("active");
-        console.log(el, el.getAttribute("id"));
+        //console.log(el, el.getAttribute("id"));
         if (el.getAttribute("id") === a.getAttribute("href").substring(1)) {
           a.classList.add("active");
         }
       });
     }
   });
+}
+
+const burger = document.querySelector(".burger");
+const inputMenu = document.querySelector(
+  ".input_menu__header_filling__navigation"
+);
+
+console.log(inputMenu);
+
+burger.addEventListener("click", onClick);
+window.addEventListener("click", e => {
+  if (
+    (e.target !== inputMenu || e.target !== burger) &&
+    inputMenu.classList.contains("act")
+  ) {
+    inputMenu.classList.remove("act");
+    inputMenu.classList.add("deact");
+  }
+});
+
+function onClick(event) {
+  event.stopPropagation();
+  if (inputMenu.classList.contains("deact")) {
+    inputMenu.classList.remove("deact");
+    inputMenu.classList.add("act");
+  } else if (inputMenu.classList.contains("act")) {
+    inputMenu.classList.remove("act");
+    inputMenu.classList.add("deact");
+  }
 }
